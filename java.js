3625d0a82de8bb037551d62f2ceefaea965fa23b
umesh2112 <umeshrocky23@gmail.com>
2025-04-03 00:41:04 +0530

    ih

--- java.js
+++ java.js
@@ -2,13 +2,44 @@ let userscore =0;
 let pcscore =0;
 
 const choices =document.querySelectorAll(".choice");
-
+console.log(choices)
+const drawgame=()=>{
+    console.log("game was draw");
+}
 // player selection rps
-const playgame = (userchoice ) =>{
+const playgame = (userchoice) =>{
     console.log("userchoice = ", userchoice);
     const computerchoice =compgen();
     console.log("computer choice =",computerchoice)
-};
+  if(userchoice==compgen){
+        drawgame();
+  } else{
+    let userwin =true;
+    if(userchoice==="rock"){
+        //computer can select scissor or paper
+       userwin = compgen=== "paper "? false :true ;
+    } else  if(userchoice==="paper"){
+        // computer can select rock and scissor
+
+        userwin = compgen === "rock" ? true:false ;
+    } else {
+        // user = sciccor 
+       // computer can generate rock and paper 
+        userwin=compgen==="rock"?false : true;
+        showinner(userwin);
+    }
+  }
+  
+}
+const showinner=(userwin)=>
+{
+    if(userwin){
+         console.log("user wins ");
+    } else {
+        console.log("user lost the game");
+    }
+} 
+
 
 // computer choice generation 
 const compgen =()=> {
@@ -17,9 +48,6 @@ const compgen =()=> {
   const randoms= Math.floor( Math.random() *3); 
   return options[randoms];
 }
-
-
-
 
 
 
